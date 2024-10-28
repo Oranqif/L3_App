e577f32ea50f27559f4197ee22e88c1e836d4603
Oranqif <23008720@myrp.edu.sg>
2024-10-28 15:38:39 +0800

    Updated

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button, Alert, TouchableOpacity, ToastAndroid} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+import React, {useState} from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoginInput/>
+      <StatusBar hidden={true} />
     </View>
   );
 }
+
+const LoginInput = () => {
+    const [pw, setPw] = useState('');
+    const [user, setUser] = useState('');
+    const [userType, setUserType] = useState('');
+    return (
+        <View>
+            <Text>User Type:</Text>
+            <RNPickerSelect onValueChange={(userTypeText) => setUserType(userTypeText)} items={[
+            {label:'Admin',value:'Admin'},
+            {label:'Guest',value:'Guest'}
+            ]}/>
+            <Text>User:</Text>
+            <TextInput style={{borderWidth: 1}} onChangeText={(userText) => setUser(userText)}></TextInput>
+            <Text>Password:</Text>
+            <TextInput style={{borderWidth: 1}} onChangeText={(pwText) => setPw(pwText)}></TextInput>
+            <TouchableOpacity onPress={() => {
+                ToastAndroid.show(`Welcome ${userType} ${user}`,ToastAndroid.SHORT)
+            }}><Text>LOG IN</Text></TouchableOpacity>
+            <Text>{pw}</Text>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
+    justifyContent: 'left',
   },
 });
